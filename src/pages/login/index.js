6e9a268e91873edbf851e51096a3898d29afe9eb
Casali <thiago.balan14@gmail.com>
2022-11-12 22:25:51 -0300

    login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Styles from './login.module.css'
 
 function index() {
   return (
-    <form>
+    <div className={Styles.loginScreen}>
+        <span>Login</span>
+        <form>
+            <div>
+            <input type="text" name="" required="" />
+            <label>Username</label>
+            </div>
+        </form>
       
-    </form>
+    </div>
   )
 }
 
